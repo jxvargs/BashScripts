b77801a82ab2d509e29d15f9072a71d5830207a5
Julian <jjulianvargas@gmail.com>
2021-09-12 19:38:10 -0700

    centos8 start script...

---
+++ centos8.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/bash
+
+# This script starts my VM through a wrapper called vagrant
+# Note: Remember, there is a defference between ./myscript vs source myscript
+#       When the script is executed, It is openingn a new shell environment.
+#       When the script is sourced, the script is typed on the current shell so
+#       it stays in the current shell as well as any changes in the environment.
+
+CENTOS8_VM=$HOME/LinuxClass/centos8
+
+cd ${CENTOS8_VM}
+
+vagrant up
+vagrant ssh
